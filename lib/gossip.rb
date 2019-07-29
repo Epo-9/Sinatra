85cd2dde895dce22928732baf1dc734edf492060
Epo-9 <eponine.paillet@gmail.com>
2019-07-29 18:40:25 +0200

    ajout exo

---
+++ lib/gossip.rb
@@ -0,0 +1,28 @@
+require 'pry'
+
+class Gossip
+
+	attr_accessor :author, :content
+
+	def initialize (author_name, content_provided)
+	@author = author_name
+	@content = content_provided
+	end
+
+	def save
+    CSV.open("./db/gossip.csv", "ab") do |csv|
+    csv << [@author, @content]
+    	end
+	end
+
+	def self.all
+    all_gossips = []
+    CSV.read("./db/gossip.csv").each do |csv_line|
+    all_gossips << Gossip.new(csv_line[0], csv_line[1])
+    end
+    return all_gossips
+    end
+
+end
+
+#binding.pry
