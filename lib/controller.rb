14cc6480b8d3fbeb8e374c300cad43aa1cbf8be0
Epo-9 <eponine.paillet@gmail.com>
2019-07-29 19:41:22 +0200

    last one

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,24 +1,27 @@
 require 'gossip'
 require 'csv'
 
+#Création d'une classe qui hérite de la classe Sinatra::Base (donc elle a les même fonctionnalité e bases que Sinatra)
 class ApplicationController < Sinatra::Base
- 	 
- 	get '/' do
- 	erb :index, locals: {gossips: Gossip.all}
+	get '/' do
+	#Permet à Sinatra de comprendre qu'il doit aller choper le fichier index qui se trouve dans views
+	erb :index, locals: {gossips: Gossip.all}
 	end
-  
-    get '/gossips/new/' do
-  	erb :new_gossip
- 	end
 
+	get	'/gossips/new/' do
+	erb	:new_gossip
+	end
+
+	#Ces lignes permettent de récupérer le nom et le contenu publié par notre "gossip man" ou "gossip girl" et de les stocker dans une BDD	
 	post '/gossips/new/' do
-    Gossip.new(params["gossip_author"], params["gossip_content"]).save
-    redirect '/'
+	
+		Gossip.new(params["gossip_author"],params["gossip_content"]).save
+		redirect '/'
 	end
-
-	post '/gossips/id' do
-	erb :show, locals: {gossips: Gossip.find(params['id'].to_i)} 
-	end	
+	#Permet de faire appel à la page show qui renvoit à l'aide d'une url dynamique le contenu d'un potin spécifique
+	get '/gossips/:id/' do
+		erb :show, locals: {gossips: Gossip.find(params['id'].to_i)} 
+	end
 
 end
 
